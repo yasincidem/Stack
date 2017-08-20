e4aba3a8ce04ead3039463abaffdf4fa8abd2e79
Yasin <yasincidem@gmail.com>
2017-08-21 01:48:04 +0300

    Add files via upload

---
+++ BalancedSymbols.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+/**
+ * Created by yasin_000 on 21.8.2017.
+ */
+public class BalancedSymbols {
+    boolean isBalanced2(String string){
+        Stack<String> stack = new Stack<>();
+        boolean balanced = true;
+        int index = 0;
+
+        while (index < string.length() && balanced){
+            String symbol = Character.toString(string.toCharArray()[index]);
+            if ("({[".contains(symbol))
+                stack.push(symbol);
+            else{
+                if (stack.isEmpty())
+                    balanced = false;
+                else{
+                    String top = stack.pop();
+                    if (!matches(top,symbol))
+                        balanced = false;
+                }
+            }
+            index++;
+        }
+        return stack.isEmpty() && balanced;
+    }
+    boolean matches(String openP, String closeP){
+        String open = "([{";
+        String close = ")]}";
+        return open.indexOf(openP) == close.indexOf(closeP);
+    }
+}
