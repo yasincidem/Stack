22ed12fd3aa476afa7af1a86a40da1f1721ddebe
Yasin <yasincidem@gmail.com>
2017-08-21 19:57:02 +0300

    Add files via upload

---
+++ DecimalToBinary.java
@@ -0,0 +1,18 @@
+/**
+ * Created by yasin_000 on 21.8.2017.
+ */
+public class DecimalToBinary {
+    static String divideby2(int decNumber){
+        StackLinkedList<Integer> stack = new StackLinkedList<>();
+
+        while (decNumber > 0){
+            int rem = decNumber % 2;
+            stack.push(rem);
+            decNumber = decNumber/2;
+        }
+        String binString = "";
+        while (!stack.isEmpty())
+            binString += Integer.toString(stack.pop());
+        return binString;
+    }
+}
